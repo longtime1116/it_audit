105e78c50f9dbfac9273992d7329e72a4995eee0
tamaiayumi <ayumi.tamai@pixta.co.jp>
2018-10-30 17:06:25 +0900

    Add function and serverless.yml

---
+++ handler.js
@@ -0,0 +1,27 @@
+'use strict';
+var repo_name = 'tamaiItAuditTest';
+var AWS = require('aws-sdk');
+var codecommit = new AWS.CodeCommit();
+
+module.exports.processing_to_processed = async (event, context) => {
+  var branch_info = {
+    branchName: 'master',
+    repositoryName: repo_name
+  };
+  codecommit.getBranch(branch_info, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else     var lastCommitId = data.branch.commitId;
+
+    var commit_info = {
+      commitId: lastCommitId,
+      repositoryName: repo_name
+    }
+    codecommit.getCommit(commit_info, function(err, data) {
+      if (err) console.log(err, err.stack);
+      else     var lastCommitComment = data.commit.message;
+      console.log(lastCommitComment);
+    })
+  });
+};
+
+
