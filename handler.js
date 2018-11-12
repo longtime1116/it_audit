4b1ea27dfcf02d37c04f7545caba2f42abf15595
tamaiayumi <ayumi.tamai@pixta.co.jp>
2018-11-12 18:10:53 +0900

    Return console.log when a head commit is revert commit

--- handler.js
+++ handler.js
@@ -6,40 +6,44 @@ var repoName = 'tamaiItAuditTest';
 
 var codecommit = new AWS.CodeCommit();
 
-module.exports.processing_to_processed = (event, context, callback) => {
-  var branchDetail = {
-    branchName: 'master',
-    repositoryName: repoName,
-  };
-  console.log(branchDetail);
+var branchDetail = {
+  branchName: 'master',
+  repositoryName: repoName,
+};
 
-  // TODO: 無駄な入れ子をなくす
+module.exports.processing_to_processed = (event, context) => {
   codecommit.getBranch(branchDetail, function(err, data) {
     if (err) {
       console.log(err, err.stack);
     } else {
       var lastCommitId = data.branch.commitId;
     }
-    console.log(lastCommitId);
 
     var commitDetail = {
       commitId: lastCommitId,
       repositoryName: repoName,
     };
+
     codecommit.getCommit(commitDetail, function(err, data) {
       if (err) {
         console.log(err, err.stack);
       } else {
         var lastCommitComment = data.commit.message;
       }
-      // ticket number for test
+
       var targetTicketName = lastCommitComment.match(/DEV-\d{1,5}/)[0];
-      // Revertが含まれたら？
-      if (lastCommitComment.match(/DEV-\d{1,5}/)) {
+      var revertComment = lastCommitComment.match(/Revert/g);
+      var revertCommentCount = revertComment ? revertComment.length : 0;
+      var isReverted = revertCommentCount % 2 == 1;
+
+      if (isReverted) {
+        console.log(targetTicketName + "'s head commit was revert commit");
+      } else if (!isReverted && lastCommitComment.match(/DEV-\d{1,5}/)) {
         updateTicketStatus(targetTicketName);
       }
     });
   });
+
   function updateTicketStatus(targetTicketName) {
     var ticketUrl =
       'https://pixta.backlog.jp/api/v2/issues/' +
@@ -56,7 +60,8 @@ module.exports.processing_to_processed = (event, context, callback) => {
         Accept: '*/*',
       },
     };
-    request.patch(data, function optionalCallback(err, httpResponse, body) {
+    request(data, function optionalCallback(err, httpResponse, body) {
+      console.log('Result in ' + targetTicketName + ':');
       console.log(body);
     });
   }
